32b5945fa4399bbdce4ba7d309df2b256e5b7736
jose2119 <josephpenafielloayza28@gmail.com>
2022-07-27 19:17:19 -0500

    settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shop',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'USER': 'codigo',
+        'PASSWORD': 'codigo2022',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
